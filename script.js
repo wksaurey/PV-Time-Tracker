34f56acd631e004dd1daec051a140fd7a9bef640
Kolter <wksaurey@gmail.com>
2024-08-08 15:31:50 -0600

    Used ai to clean up html and js

--- script.js
+++ script.js
@@ -1,111 +1,73 @@
-let pinky_button = document.getElementById('pinky-button')
-let brain_button = document.getElementById('brain-button')
-let track_button = document.getElementById('track-button')
-let pinky_time = document.getElementById('pinky-time');
-let brain_time = document.getElementById('brain-time');
-let track_time = document.getElementById('track-time');
+// Element references
+const buttons = {
+  pinky: document.getElementById('pinky-button'),
+  brain: document.getElementById('brain-button'),
+  track: document.getElementById('track-button'),
+};
 
-let pinky_start, 
-	brain_start, 
-	track_start;
+const times = {
+  pinky: document.getElementById('pinky-time'),
+  brain: document.getElementById('brain-time'),
+  track: document.getElementById('track-time'),
+};
 
-pinky_button.addEventListener('click', () => {
-	if (pinky_start) {
-		pinky_button.innerHTML = 'Start'
-		pinky_button.classList.remove('stop');
-		pinky_start = null;
-		pinky_time.innerHTML = 0;
-	} else {
-		pinky_start = new Date().getTime();
-	}
-})
+let startTimes = {
+  pinky: null,
+  brain: null,
+  track: null,
+};
 
-brain_button.addEventListener('click', () => {
-	brain_start = new Date().getTime();
-})
+// Event listener setup
+Object.keys(buttons).forEach(key => {
+  buttons[key].addEventListener('click', () => {
+    toggleTimer(key);
+  });
+});
 
-track_button.addEventListener('click', () => {
-	track_start = new Date().getTime();
-})
+// Toggle timer function
+function toggleTimer(key) {
+  if (startTimes[key]) {
+    buttons[key].innerHTML = 'Restart';
+    buttons[key].classList.remove('stop');
+    startTimes[key] = null;
+  } else {
+    startTimes[key] = new Date().getTime();
+  }
+}
 
-var update_time_interval = setInterval(() => {
-	let now = new Date().getTime();
-	if (pinky_start) {
-		//update buttons
-		pinky_button.innerHTML = "Stop"
-		pinky_button.classList.add('stop')
-
-		// update time
-		let time = now - pinky_start;
-		let time_info = get_time(time);
-		let hours = time_info.get('hours')
-		let minutes = time_info.get('minutes')
-		let seconds = time_info.get('seconds')
-		if (hours) {
-			pinky_time.innerHTML = `${hours}:${minutes}:${seconds}`;
-		} else if (minutes) {
-			pinky_time.innerHTML = `${minutes}:${seconds}`;
-		} else {
-			pinky_time.innerHTML = `${seconds}`;
-		}
-	}
-	
-	if (brain_start) {
-		// update buttons
-		brain_button.innerHTML = "Stop"
-
-		// update time
-		let time = now - brain_start;
-		let time_info = get_time(time);
-		let hours = time_info.get('hours')
-		let minutes = time_info.get('minutes')
-		let seconds = time_info.get('seconds')
-		if (hours) {
-			brain_time.innerHTML = `${hours}:${minutes}:${seconds}`;
-		} else if (minutes) {
-			brain_time.innerHTML = `${minutes}:${seconds}`;
-		} else {
-			brain_time.innerHTML = `${seconds}`;
-		}
-	}
-
-	if (track_start) {
-		// update buttons
-		track_button.innerHTML = "Stop"
-
-
-		// update time
-		let time = now - track_start;
-		let time_info = get_time(time);
-		let hours = time_info.get('hours')
-		let minutes = time_info.get('minutes')
-		let seconds = time_info.get('seconds')
-		if (hours) {
-			track_time.innerHTML = `${hours}:${minutes}:${seconds}`;
-		} else if (minutes) {
-			track_time.innerHTML = `${minutes}:${seconds}`;
-		} else {
-			track_time.innerHTML = `${seconds}`;
-		}
-	}
+// Update timer intervals
+setInterval(() => {
+  const now = new Date().getTime();
+  
+  Object.keys(startTimes).forEach(key => {
+    if (startTimes[key]) {
+      buttons[key].innerHTML = "Stop";
+      buttons[key].classList.add('stop');
+      
+      const timeElapsed = now - startTimes[key];
+      const timeInfo = getTimeInfo(timeElapsed);
+      
+      times[key].innerHTML = formatTime(timeInfo);
+    }
+  });
 }, 1000);
 
-function get_time(time) {
+// Get time info
+function getTimeInfo(time) {
+  const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((time % (1000 * 60)) / 1000);
+  
+  return { hours, minutes, seconds };
+}
 
-	let hours = Math.floor(
-	(time % (1000 * 60 * 60 * 24)) /
-	(1000 * 60 * 60)
-	);
-	let minutes = Math.floor(
-	(time % (1000 * 60 * 60)) / (1000 * 60)
-	);
-	let seconds = Math.floor(
-	(time % (1000 * 60)) / 1000
-	);
-	const time_info = new Map([
-	['hours', hours],
-	['minutes', minutes],
-	['seconds', seconds]
-	])
-	return time_info;
+// Format time for display
+function formatTime({ hours, minutes, seconds }) {
+  if (hours) {
+    return `${hours}:${minutes}:${seconds}`;
+  } else if (minutes) {
+    return `${minutes}:${seconds}`;
+  } else {
+    return `${seconds}`;
+  }
 }
