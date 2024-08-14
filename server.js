a72873e8c537d6f1256d68763cbe09cf7876d41d
Kolter <wksaurey@gmail.com>
2024-08-14 15:40:26 -0600

    started node server with express as framework

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const {readFile, readFileSync} = require('fs').promises;
+const app = express();
+app.use(express.static('static'));
+
+const PORT = 3000;
+app.listen(PORT, () => {
+	  console.log(`Server running at http://localhost:${PORT}/`);
+});
+
+
+// app.get('/', async (request, response) => {
+
+// 	response.send(await readFile('./index.html', 'utf8'));
+
+// });
