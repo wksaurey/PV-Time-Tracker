99b0179f3300d891cb80e36bc9dfa7211b45dc0d
Kolter <wksaurey@gmail.com>
2024-08-15 14:32:20 -0600

    restructured file structure

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const {readFile, readFileSync} = require('fs').promises;
 const app = express();
-app.use(express.static('static'));
+app.use(express.static('../client'));
 
 const PORT = 3000;
 app.listen(PORT, () => {
